f306ce0507c389247957f05c2a3c70c3d134ae02
Tanush <correct-email@example.com>
2025-05-26 22:20:03 +0530

    step 3 prepared the models and schemas for the mongodb and created the admin route

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 const app = express();
 
-app.use("/user", userRouter); //any requrest on the user will divert to the userRouter and that will handle further paths
-app.use("/course", courseRouter); //any requrest on the course will divert to courseRouter and it will handle further 
-
-createUserRoutes(app);
-createCourseRoutes(app);
+app.use("/user", userRouter); //any request on the user will divert to the userRouter and that will handle further paths
+app.use("/course", courseRouter); //any request on the course will divert to courseRouter and it will handle further 
+app.use("/admin", adminRouter); //any request on the admin will divert to adminRouter and it will handle further 
 
 app.listen(3000);
